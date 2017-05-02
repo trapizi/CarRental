078fda614c61e1a1ce874dcfd2f304901f91074f
z5020069 <z5020069@ad.unsw.edu.au>
2017-05-02 01:59:58 +0000

    Adjustment.java created online with Bitbucket

---
+++ Adjustment.java
@@ -0,0 +1,78 @@
+public class Adjustment {
+	//private Member seeker;
+	//private Member offerer;
+	private String status;
+	private String payAmt;
+	private String agreeDate;
+	private String uniqueNo;
+	private String createDay;
+	private long toPin;
+	private long fromPin;
+	private long pUpFrom;
+	private long pUpTo;
+	private String day;
+	//private Member initiatedBy;
+
+	
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public String getPayAmt() {
+		return payAmt;
+	}
+	public void setPayAmt(String payAmt) {
+		this.payAmt = payAmt;
+	}
+	public String getAgreeDate() {
+		return agreeDate;
+	}
+	public void setAgreeDate(String agreeDate) {
+		this.agreeDate = agreeDate;
+	}
+	public String getUniqueNo() {
+		return uniqueNo;
+	}
+	public void setUniqueNo(String uniqueNo) {
+		this.uniqueNo = uniqueNo;
+	}
+	public String getCreateDay() {
+		return createDay;
+	}
+	public void setCreateDay(String createDay) {
+		this.createDay = createDay;
+	}
+	public long getToPin() {
+		return toPin;
+	}
+	public void setToPin(long toPin) {
+		this.toPin = toPin;
+	}
+	public long getFromPin() {
+		return fromPin;
+	}
+	public void setFromPin(long fromPin) {
+		this.fromPin = fromPin;
+	}
+	public long getpUpFrom() {
+		return pUpFrom;
+	}
+	public void setpUpFrom(long pUpFrom) {
+		this.pUpFrom = pUpFrom;
+	}
+	public long getpUpTo() {
+		return pUpTo;
+	}
+	public void setpUpTo(long pUpTo) {
+		this.pUpTo = pUpTo;
+	}
+	public String getDay() {
+		return day;
+	}
+	public void setDay(String day) {
+		this.day = day;
+	}
+
+}
