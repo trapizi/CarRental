658509aeea607950b07b0ffdb4f36218753e3626
trapizi <stevenngo97@gmail.com>
2017-05-10 06:37:43 +0000

    carSeek.java created online with Bitbucket

---
+++ Car Share Office/carSeek.java
@@ -0,0 +1,16 @@
+package suber.CarShareOffice;
+
+import suber.User.Member;
+
+public class carSeek {
+    private Member ownerMember;
+
+    
+    public Member getOwnerMember() {
+        return ownerMember;
+    }
+
+    public void setOwnerMember(Member ownerMember) {
+        this.ownerMember = ownerMember;
+    }
+}
