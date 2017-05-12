2b1a102db505e09cc354fb372baedd0898d9fdad
Bing Wen <z3463269@zmail.unsw.edu.au>
2017-05-12 18:58:08 +1000

    staff, member, user and corporate member

---
+++ project/model/CorporateMember.java
@@ -0,0 +1,13 @@
+package project.model;
+
+public class CorporateMember extends Member {
+	private Corporate corporation;
+
+	public Corporate getCorporation() {
+		return corporation;
+	}
+
+	public void setCorporation(Corporate corporation) {
+		this.corporation = corporation;
+	}
+}
---
+++ project/model/Staff.java
@@ -0,0 +1,22 @@
+package project.model;
+
+public class Staff extends User {
+	private int staff_id;
+	private double salary;
+
+	public int getStaff_id() {
+		return staff_id;
+	}
+
+	public void setStaff_id(int staff_id) {
+		this.staff_id = staff_id;
+	}
+	
+	public double getSalary() {
+		return salary;
+	}
+	
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+}
