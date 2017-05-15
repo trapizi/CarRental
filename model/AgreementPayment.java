397fd2949a9d08edbba12b61331a7f3f56a5d411
Selena Tse <onemusic_onelove>
2017-05-15 22:47:13 +1000

    add some stuff

--- model/AgreementPayment.java
+++ model/AgreementPayment.java
@@ -1,5 +1,14 @@
 package model;
 
 public class AgreementPayment extends Payment{
+    	private int agreementPaymentID;
+
+	public int getAgreementPaymentID() {
+		return agreementPaymentID;
+	}
+
+	public void setAgreementPaymentID(int agreementPaymentID) {
+		this.agreementPaymentID = agreementPaymentID;
+	}
     
 }
--- model/Payment.java
+++ model/Payment.java
@@ -12,21 +12,6 @@ public class Payment {
     private String accountOwnerName;
     private String paymentMedia;
 
-    //PAYMENT constructor
-    /*public Payment() {
-        this.amount = new amount();
-        this.first_name = new SimpleStringProperty();
-        this.last_name = new SimpleStringProperty();
-        this.email = new SimpleStringProperty();
-        this.phone_number = new SimpleStringProperty();
-        this.hire_date = new SimpleObjectProperty<>();
-        this.job_id = new SimpleStringProperty();
-        this.salary = new SimpleIntegerProperty();
-        this.commission_pct = new SimpleDoubleProperty();
-        this.manager_id = new SimpleIntegerProperty();
-        this.department_id = new SimpleIntegerProperty();
-    } */
-    
     public double getAmount(){
         return amount;
     }
