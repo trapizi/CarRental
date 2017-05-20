496e74f21e05866baf6fbd37ff99a8eeec7ff8bd
Selena Tse <onemusic_onelove>
2017-05-20 15:50:50 +1000

    finalised consultationPayment and consultationPaymentList

--- model/ConsultationPayment.java
+++ model/ConsultationPayment.java
@@ -1,15 +1,34 @@
 package model;
 
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+
 public class ConsultationPayment extends Payment {
     
-    	private int consultantPaymentID;
-
-	public int getConsultantPaymentID() {
-		return consultantPaymentID;
-	}
-
-	public void setConsultantPaymentID(int consultantPaymentID) {
-		this.consultantPaymentID = consultantPaymentID;
-	}
+    //CONSULTATION_PAYMENT table columns
+    private IntegerProperty consultationPayment_id;
     
+    //CONSULTATION_PAYMENT default constructor
+    public ConsultationPayment() {
+        this.consultationPayment_id = new SimpleIntegerProperty();
+    }
+    
+    //return string containing consultationPaymentID + other payment details
+    public String toString(){
+        return "ConsultationPayment_id: " + this.getConsultationPayment_id() + " | " + super.toString();
+    }
+    
+    //agreementPaymentID
+    public Integer getConsultationPayment_id() {
+	return consultationPayment_id.get();
+    }
+
+    public void setConsultationPayment_id(Integer consultationPayment_id) {
+	this.consultationPayment_id.set(consultationPayment_id);
+    }
+    
+    public IntegerProperty consultationPayment_idProperty(){
+        return consultationPayment_id;
+    }
+
 }
